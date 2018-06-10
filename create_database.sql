cee164ec1b0f78fc3c78821c52b903f31afd0c2d
Juan Pablo Orsay <juan.orsay@avature.net>
2018-06-10 18:22:25 -0300

    Add create_database script

---
+++ create_database.sql
@@ -0,0 +1,100 @@
+DROP DATABASE IF EXISTS vast3;
+DROP USER IF EXISTS infoviz;
+
+CREATE USER infoviz WITH PASSWORD 'infoviz' CREATEDB;
+CREATE DATABASE vast3 OWNER infoviz;
+
+\c vas3 infoviz;
+SET ROLE 'infoviz';
+
+CREATE TABLE company_index (
+    name VARCHAR,
+    surname VARCHAR,
+    employer_id integer NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE etype (
+    id int PRIMARY KEY,
+    type VARCHAR(20)
+);
+
+INSERT INTO etype (id, type) VALUES 
+    (0, 'call'),
+    (1, 'email'),
+    (2, 'purchases'),
+    (3, 'meetings')
+;
+
+CREATE TABLE calls (
+    source_id int NOT NULL REFERENCES company_index(employer_id),
+    etype_id int NOT NULL REFERENCES etype(id),
+    target_id int NOT NULL REFERENCES company_index(employer_id),
+    created_at bigint NOT NULL
+);
+
+CREATE TABLE suspicious_calls (
+    source_id int NOT NULL REFERENCES company_index(employer_id),
+    etype_id int NOT NULL REFERENCES etype(id),
+    target_id int NOT NULL REFERENCES company_index(employer_id),
+    created_at bigint NOT NULL
+);
+
+CREATE TABLE purchases (
+    source_id int NOT NULL REFERENCES company_index(employer_id),
+    etype_id int NOT NULL REFERENCES etype(id),
+    target_id int NOT NULL REFERENCES company_index(employer_id),
+    created_at bigint NOT NULL
+);
+
+CREATE TABLE suspicious_purchases (
+    source_id int NOT NULL REFERENCES company_index(employer_id),
+    etype_id int NOT NULL REFERENCES etype(id),
+    target_id int NOT NULL REFERENCES company_index(employer_id),
+    created_at bigint NOT NULL
+);
+
+CREATE TABLE other_suspicious_purchases (
+    source_id int NOT NULL REFERENCES company_index(employer_id),
+    etype_id int NOT NULL REFERENCES etype(id),
+    target_id int NOT NULL REFERENCES company_index(employer_id),
+    created_at bigint NOT NULL
+);
+
+CREATE TABLE emails (
+    source_id int NOT NULL REFERENCES company_index(employer_id),
+    etype_id int NOT NULL REFERENCES etype(id),
+    target_id int NOT NULL REFERENCES company_index(employer_id),
+    created_at bigint NOT NULL
+);
+
+CREATE TABLE suspicious_emails (
+    source_id int NOT NULL REFERENCES company_index(employer_id),
+    etype_id int NOT NULL REFERENCES etype(id),
+    target_id int NOT NULL REFERENCES company_index(employer_id),
+    created_at bigint NOT NULL
+);
+
+CREATE TABLE meetings (
+    source_id int NOT NULL REFERENCES company_index(employer_id),
+    etype_id int NOT NULL REFERENCES etype(id),
+    target_id int NOT NULL REFERENCES company_index(employer_id),
+    created_at bigint NOT NULL
+);
+
+CREATE TABLE suspicious_meetings (
+    source_id int NOT NULL REFERENCES company_index(employer_id),
+    etype_id int NOT NULL REFERENCES etype(id),
+    target_id int NOT NULL REFERENCES company_index(employer_id),
+    created_at bigint NOT NULL
+);
+
+\COPY company_index FROM 'data/CompanyIndex.csv' CSV HEADER;
+\COPY calls FROM 'data/calls.csv' CSV;
+\COPY suspicious_calls FROM 'data/Suspicious_calls.csv' CSV;
+\COPY emails FROM 'data/emails.csv' CSV;
+\COPY suspicious_emails FROM 'data/Suspicious_emails.csv' CSV;
+\COPY meetings FROM 'data/meetings.csv' CSV;
+\COPY suspicious_meetings FROM 'data/Suspicious_meetings.csv' CSV;
+\COPY purchases FROM 'data/purchases.csv' CSV;
+\COPY suspicious_purchases FROM 'data/Suspicious_purchases.csv' CSV;
+\COPY other_suspicious_purchases FROM 'data/Other_suspicious_purchases.csv' CSV;
