78624729054e4e43b15555257cb720e020f5722b
Juan Pablo Orsay <juan.orsay@avature.net>
2018-06-23 23:52:05 -0300

    Query to export data

---
+++ setup/99_data_creation.sql
@@ -0,0 +1,1 @@
+COPY (SELECT source_id, target_id, SUM(total) as interactions FROM employee_communication_split GROUP BY source_id, target_id) TO '/tmp/aggregated_interactions.csv' WITH CSV DELIMITER ',';
